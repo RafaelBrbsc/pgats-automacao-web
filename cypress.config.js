32a3d9d8e8e9b110f791307f522254ddee76b8b6
unknown <rafaelbrbsc@gmail.com>
2025-10-30 00:23:02 -0300

    configura mochawesome report plugin dentro do cypress

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,8 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on)
     },
   },
-  reporter: 'mochawesome',
+  reporter: 'cypress-mochawesome-reporter',
 });
